08dec4ca72116b34c5a2b3c8ecbaab6f36bbb7c6
Nicholas A <nickarsenault18@gmail.com>
2024-01-02 01:46:21 -0500

    Update gui.py
    added title label, description, changed layout, and setup for the "spawning" of the mosaic itself. 
    globalized desc to allow for changing, imported full mosaic.py.
    (working)

--- gui.py
+++ gui.py
@@ -2,8 +2,10 @@
 
 import tkinter as tk
 from tkinter import filedialog
-from mosaic import generate_mosaic
+from mosaic import *
 import patoolib
+
+mosaic_description: tk.StringVar
 
 
 # Uploading the images
@@ -18,16 +20,33 @@ def upload_images():
 
 
 def extract_images(file_path):
+    global mosaic_description
+    
     # Implement code to extract images from the archive file and store them in a folder
     extract_folder = "extracted_images"
     patoolib.extract_archive(file_path, outdir=extract_folder)
 
+    mosaic_description.set(random_phrase(mosaic_feedback_phrases))    # changes phrases randomly after upload
+
 
 def create_gui():
+    global mosaic_description
+    
     app = tk.Tk()
     app.title("MosaicMeMagic")
+    app.geometry("800x800")
+    app.configure(bg="#f0f0f0")
 
     # Add GUI elements here
+    label = tk.Label(app, text="Welcome to MosaicMeMagic!", font=("Arial", 20), bg="#f0f0f0", pady=10)
+    label.pack(side="top")
+
+    mosaic_description = tk.StringVar()
+    mosaic_description.set("Upload a zipped '.zip', '.7z', or '.rar' folder with some images inside. \nYour mosaic will appear here.")
+
+    mosaic_label = tk.Label(app, textvariable=mosaic_description, bg="#f0f0f0", pady=10, font=("Arial", 15))
+    mosaic_label.pack(side="top")
+    
     upload_button = tk.Button(app, text="Upload Images", command=upload_images)
     upload_button.pack()
 
