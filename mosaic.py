6d7d67a3c911825d73bba4ac7c9283c65db0eb36
Nicholas A <nickarsenault18@gmail.com>
2024-01-02 00:51:24 -0500

    Create mosaic.py
    Pillow import for later use, setup for actual mosaic logic

---
+++ mosaic.py
@@ -0,0 +1,11 @@
+# mosaic only
+
+from PIL import Image
+import os
+
+
+def generate_mosaic(images_folder, base_image_path, tile_size):
+    # Implement code to generate mosaic from images in new folder
+    pass
+
+# Whatever else
