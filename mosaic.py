6637c9a63b86700397c804d2a73fd01db127cf45
Nicholas A <nickarsenault18@gmail.com>
2024-01-02 01:38:05 -0500

    Update mosaic.py
    added random_phrase grabber with 10 phrases to display after successful upload of files (working)

--- mosaic.py
+++ mosaic.py
@@ -2,10 +2,26 @@
 
 from PIL import Image
 import os
+import random
+
+mosaic_feedback_phrases = [
+    "You just crafted a masterpiece! Ready for more?",
+    "Your mosaic game is strong! Want another round?",
+    "Impressive mosaic skills! How about one more try?",
+    "Wow, you're a mosaic maestro! Ready for another creation?",
+    "That mosaic is a work of art! Want to try your luck again?",
+    "You've got the mosaic magic! Up for another challenge?",
+    "Bravo! Your mosaic skills are top-notch. Ready for more fun?",
+    "A true mosaic marvel! Want to create another masterpiece?",
+    "Your mosaic mojo is unbeatable! Ready for another spin?",
+    "Mosaic excellence achieved! Want to give it another shot?"
+]
 
 
 def generate_mosaic(images_folder, base_image_path, tile_size):
     # Implement code to generate mosaic from images in new folder
     pass
 
-# Whatever else
+def random_phrase(phrases):
+    chosen_phrase = random.choice(phrases)
+    return chosen_phrase
